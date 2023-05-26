2301fa0dc1bf08a37a7fe9cdaf860e818009c185
cellamo <zaferarslaan@gmail.com>
2023-05-26 15:07:44 +0300

    x

--- 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ 02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -7,18 +7,14 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"),
-        new Grade("Hermonie", "Aritchmancy", "B+"),
-        new Grade("Zafer", "Software", "A+")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        
         model.addAttribute("grades", studentGrades);
         return "grades";
     }
@@ -29,4 +25,15 @@ public class GradeController {
         return "form";
     }
 
-}
+    @GetMapping("/")
+    public String gradeForm(Model model) {
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
+    @PostMapping("/handleSubmit")
+    public String submitForm(Grade grade){
+        studentGrades.add(grade);
+        return "redirect:/grades";
+    }
+    }
